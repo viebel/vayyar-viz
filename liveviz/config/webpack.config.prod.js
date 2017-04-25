7f95943f91c63c12effc3ef1b24e6113eab9b3b1
Yehonathan Sharvit <viebel@gmail.com>
2017-04-25 16:50:05 +0300

    experiment with golden-layout

--- liveviz/config/webpack.config.prod.js
+++ liveviz/config/webpack.config.prod.js
@@ -82,9 +82,7 @@ module.exports = {
     alias: {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
-	'react-native': 'react-native-web',
-     // https://github.com/seiyria/bootstrap-slider#how-do-i-exclude-the-optional-jquery-dependency-from-my-build
-	"jquery": paths.appSrc + "/jquery-stub.js"	
+	'react-native': 'react-native-web'
     }
   },
   
--- liveviz/src/index.js
+++ liveviz/src/index.js
@@ -1,9 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import GoldenLayout from 'golden-layout';
+import $ from 'jquery';
+import 'golden-layout/src/css/goldenlayout-base.css';
+import 'golden-layout/src/css/goldenlayout-dark-theme.css';
 import './index.css';
 
-ReactDOM.render(
+/*ReactDOM.render(
   <App />,
   document.getElementById('root')
-);
+);*/
+console.log("$: " + $);
+window.ReactDOM = ReactDOM;
+window.React = React;
+
+console.log("ReactDOM: " + ReactDOM.render);
+var myLayout = new GoldenLayout({
+  content: [{
+    type: 'row',
+    content:[{
+      type:'react-component',
+      component: 'test-component',
+      props: { label: 'A' }
+    },{
+      type: 'column',
+      content:[{
+        type:'react-component',
+        component: 'test-component',
+        props: { label: 'B' }
+      },{
+        type:'react-component',
+        component: 'test-component',
+        props: { label: 'C' }
+      }]
+    }]
+  }]
+});
+
+var TestComponent = React.createClass({
+ render: function() {
+   return (<h1>{this.props.label}</h1>)
+ }
+});
+
+myLayout.registerComponent( 'test-component', TestComponent );
+
+//Once all components are registered, call
+myLayout.init();
