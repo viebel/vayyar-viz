60bbdbe83207a4d34bda63d9f4eb2f3c78fb095f
Yehonathan Sharvit <viebel@gmail.com>
2017-05-07 17:48:00 +0300

    move code from HeatMapUI to HeatMap

--- liveviz/src/logic/HeatMap.js
+++ liveviz/src/logic/HeatMap.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import HeatMapUI from '../ui/HeatMapUI';
+import FetchPeriodic from '../common/FetchPeriodic';
+
 import {assoc} from 'ramda';
 
 
@@ -14,15 +16,31 @@ class HeatMap extends Component {
     this.setState(assoc('error', `Cannot connect to server at ${url}`, this.state));
   }
   render() {
+    const {updateStatus, status, running, url} = this.props;
     return (
       <div>
-      <div> {this.state.error} </div>
-      <HeatMapUI
-      {...this.props}
-      onError={this.onError}/>
+        <div>running:{this.props.running.toString()}</div>
+        <div> {this.state.error} </div>
+        {status === "disconnected"? null :
+          <div>
+            <FetchPeriodic
+              url={`${url}/demoData2`}
+              onAnimationFrame={true}
+              prevent={!running}
+              onSuccess={data => {
+                this.setState(assoc('data', data, this.state));
+                updateStatus("connected");
+              }}
+              onError={this.onError}
+              />
+            <HeatMapUI
+              data={this.state.data}
+              />
+          </div>
+        }
       </div>
-    );
+      );
+    }
   }
-}
 
-export default HeatMap;
+  export default HeatMap;
--- liveviz/src/ui/HeatMapUI.js
+++ liveviz/src/ui/HeatMapUI.js
@@ -1,36 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import drawHeatMap from '../heatmap/heatmap';
-import FetchPeriodic from '../common/FetchPeriodic';
 
-const HeatMapUI =({updateStatus, status, onError, running, error, url}) => {
-  let canvas = null;
-  const draw = (data) => drawHeatMap(canvas, data.Data, {min: 0, max: 4000})
-
-  return (
-    <div>
-      <div>{error}</div>
-      <div>running:{running.toString()}</div>
-
-      {status === "disconnected"? null :
-        <div>
-          <FetchPeriodic
-            url={`${url}/demoData2`}
-            onAnimationFrame={true}
-            prevent={!running}
-            onSuccess={data => {
-              draw(data);
-              updateStatus("connected");
-            }}
-            onError={onError}
-            />
-          <canvas
-            width="600"
-            height="400"
-            ref={element => canvas = element}/>
-        </div>
-      }
-    </div>
-  );
+class HeatMapUI extends Component {
+  draw (data) {
+    drawHeatMap(this.canvas, data.Data, {min: 0, max: 4000});
+  }
+  render () {
+    if(this.props.data && this.canvas) {
+      this.draw(this.props.data);
+    }
+    return (
+      <canvas
+      width="600"
+      height="400"
+      ref={element => this.canvas = element}/>
+    );
+  }
 }
 
 export default HeatMapUI;
