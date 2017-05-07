8e43ac75bbec2bd5cbf1734bc68ddf8a203d3a7d
Yehonathan Sharvit <viebel@gmail.com>
2017-05-07 17:57:07 +0300

    move code from TrackerUI -> Tracker

---
+++ liveviz/src/logic/Tracker.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import FetchPeriodic from './common/FetchPeriodic';
+import TrackerUI from '../ui/TrackerUI';
+import {assoc} from 'ramda';
+
+class Tracker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      targets: [],
+    }
+  }
+  render() {
+    return (
+      <div>
+        <div>{this.state.error}</div>
+        <div>running:{this.props.running.toString()}</div>
+        {this.props.status === "disconnected"? null :
+          <FetchPeriodic
+            url={`${this.props.url}/Targets`}
+            onAnimationFrame={true}
+            prevent={!this.props.running}
+            onSuccess={data => {
+              this.setState(assoc('targets', data.targets, this.state));                this.props.updateStatus("connected");
+            }}
+            onError={(reason, url) => {
+              this.props.updateStatus("error");
+              this.setState(assoc('error', `Cannot connect to server at ${url}`, this.state));
+            }}
+            />
+        }
+        <TrackerUI targets={this.state.targets}/>
+      </div>
+    );
+  }
+}
+
+export default Tracker;
---
+++ liveviz/src/ui/TrackerUI.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import {merge} from 'ramda';
+import './styles/tracker.css';
+
+const Target = ({x, y, type}) => {
+  const targetClasses = {
+    "circle": "target-circle",
+    "square": "target-square",
+    "triangle": "target-triangle",
+  };
+  const targetClass = targetClasses[type] || "target-unknown";
+  return (
+    <div className={`target-arena ${targetClass}`}
+      style={{top: y, left: x}}/>
+  );
+}
+
+class TrackerUI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: 0,
+      height: 0,
+    }
+  }
+  componentDidMount() {
+    const {width, height} = this.domElement.getBoundingClientRect();
+    this.setState(merge(this.state, {width, height}));
+  }
+  render() {
+    const {width, height} = this.state,
+    {targets} = this.props,
+    [arenaWidth, arenaHeight] = [20, 10];
+
+    return (
+      <div className="tracker-arena"
+        ref={element => this.domElement = element}>
+        {
+          targets.map (target =>
+            <Target
+              key={target.TargetType + "__" + target.Name}
+              type={target.TargetType}
+              x={(target.X + Math.random())* width/arenaWidth}
+              y={(target.Y + Math.random())* height/arenaHeight}
+              />
+          )
+        }
+      </div>
+    );
+  }
+}
+
+export default TrackerUI;
