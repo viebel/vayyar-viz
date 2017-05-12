576ae962e3f1435a2f85c49d0f33c4743b8a176a
Yehonathan Sharvit <viebel@gmail.com>
2017-05-12 14:01:12 +0300

    introduce thunkMiddleware

--- liveviz/src/index.js
+++ liveviz/src/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './logic/App'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { createStore, compose, applyMiddleware } from 'redux'
 import app  from './reducers/index'
 import './styles/index.css'
 
-let store = createStore(app,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(app, composeEnhancers(
+  applyMiddleware(thunkMiddleware))
 )
 
 ReactDOM.render(
