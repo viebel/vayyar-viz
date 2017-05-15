d7af23e6ce8cbdd8a6437427d3d5bc14c09b1096
Yehonathan Sharvit <viebel@gmail.com>
2017-05-15 07:40:18 +0300

    split reducers

---
+++ liveviz/src/reducers/DataReducers.js
@@ -0,0 +1,27 @@
+import { assocPath, assoc, findIndex, adjust, propEq } from 'ramda'
+
+const defaultState = {
+  tracker: {targets:[]},
+  heatmap: [],
+  params: {variables: []},
+}
+
+const updateParam = (params, name, value) => {
+  const idx = findIndex(propEq('ActualName', name), params)
+  return adjust(assoc('Value',value), idx)(params)
+}
+
+export const data = (state=defaultState, action) => {
+  switch(action.type) {
+    case 'DATA_UPDATE_TRACKER':
+    return assoc('tracker', action.val, state)
+    case 'DATA_UPDATE_HEATMAP':
+    return assoc('heatmap', action.val, state)
+    case 'DATA_UPDATE_PARAMS':
+    return assoc('params', action.val, state)
+    case 'DATA_UPDATE_PARAM':
+    return assocPath(['params', 'variables'], updateParam(state.params.variables, action.val.name, action.val.value), state)
+    default:
+    return state
+  }
+}
---
+++ liveviz/src/reducers/GlobalReducers.js
@@ -0,0 +1,18 @@
+import { merge, assoc } from 'ramda'
+
+const defaultState = {
+  serverRoot: 'http://209.9.36.2:1234',
+  connectionStatus: 'disconnected',
+  graphKey: 0,
+}
+
+export const global = (state=defaultState, action) => {
+  switch(action.type) {
+    case 'SET_SERVER_ROOT':
+    return merge(state, {serverRoot: action.val, graphKey: state.graphKey + 1})
+    case 'SET_CONNECTION_STATUS':
+    return assoc('connectionStatus', action.val, state)
+    default:
+    return state
+  }
+}
