0c4f4334384e5428dee8ec77655c14e6289e7a05
Yehonathan Sharvit <viebel@gmail.com>
2017-05-19 15:05:36 +0300

    button to hide params

--- liveviz/src/logic/GraphAndParams.js
+++ liveviz/src/logic/GraphAndParams.js
@@ -1,18 +1,24 @@
 import { connect } from 'react-redux'
 import { trackerScreenToggleRunning} from '../actions/TrackerScreenActions'
 import GraphAndParamsUI from '../ui/GraphAndParamsUI';
+import { toggleParamsDisplay} from '../actions/ParamsScreenActions'
+
 
 const mapDispatchToProps = (dispatch) => {
   return {
     toggleRunning: () => {
       dispatch(trackerScreenToggleRunning())
-    }
+    },
+    toggleParams: () => {
+      dispatch(toggleParamsDisplay())
+    },
   }
 }
 
 const mapStateToProps = (state) => {
   return {
     running: state.screens.tracker.running,
+    displayParams: state.screens.params.display
   }
 }
 
