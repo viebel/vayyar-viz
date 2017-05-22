b7d79af19c710b64d5d3999b7b056bb971499be5
Yehonathan Sharvit <viebel@gmail.com>
2017-05-22 11:26:46 +0300

    test for Slider

--- liveviz/src/controls/Slider.test.js
+++ liveviz/src/controls/Slider.test.js
@@ -22,6 +22,8 @@ describe('Slider', () => {
     Description: "A cool param",
     ActualName: "A cool name",
     Value: 18,
+    Min: 10,
+    Max: 30
   }
   beforeEach(() => {
     const s = setup(args)
@@ -35,11 +37,26 @@ describe('Slider', () => {
   it('should render a FormControl of type number', () => {
     const elem = wrapper.find('FormControl').first()
     expect(elem.props().type).toBe('number')
+    expect(elem.props().min).toBe(args.Min)
+    expect(elem.props().max).toBe(args.Max)
     expect(elem.props().value).toBe(args.Value)
   })
-  it('should call onChange with `false` when clicking on second option', () => {
-    const elem = wrapper.find('FormControl').first()
+  it('should call FormControl onChange with proper value', () => {
+    const elem = wrapper.find('FormControl')
     elem.props().onChange({target:{value: 98}})
+    expect(props.onChange.mock.calls.length).toBe(1)
+    expect(props.onChange.mock.calls[0]).toEqual([98])
+  })
+  it('should render a ReactBootstrapSlider', () => {
+    const elem = wrapper.find('ReactBootstrapSlider')
+    expect(elem.props().min).toBe(args.Min)
+    expect(elem.props().max).toBe(args.Max)
+    expect(elem.props().value).toBe(args.Value)
+    expect(elem.props().tooltip).toBe("hide")
+  })
+  it('should call ReactBootstrapSlider onChange with proper value', () => {
+    const elem = wrapper.find('ReactBootstrapSlider')
+    elem.props().change({target:{value: 98}})
     expect(props.onChange.mock.calls.length).toBe(1)
     expect(props.onChange.mock.calls[0]).toEqual([98])
   })
