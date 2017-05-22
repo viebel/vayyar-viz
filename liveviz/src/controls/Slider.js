c1c006a63da09be7ee59d015642d3e452bb78c0c
Yehonathan Sharvit <viebel@gmail.com>
2017-05-22 12:34:47 +0300

    New Control: Number (#24)

--- liveviz/src/controls/Slider.js
+++ liveviz/src/controls/Slider.js
@@ -1,38 +1,31 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {FormGroup, FormControl, ControlLabel, Col} from 'react-bootstrap';
 import ReactBootstrapSlider from 'react-bootstrap-slider';
 import 'bootstrap-slider/dist/css/bootstrap-slider.css';
 
+const Slider = ({args, onChange}) =>
+<FormGroup >
+  <Col componentClass={ControlLabel} xs={12}>
+    {args.Description}
+  </Col>
+  <Col xs={12}>
+    <FormControl
+      type="number"
+      min={args.Min}
+      max={args.Max}
+      value={args.Value}
+      onChange={(e) => onChange(e.target.value)}/>
+  </Col>
+  <Col xs={12}></Col>
+  <Col xs={12}>
+    <ReactBootstrapSlider
+      value={args.Value}
+      min={args.Min}
+      max={args.Max}
+      tooltip="hide"
+      change={(e) => onChange(e.target.value)}
+      />
+  </Col>
+</FormGroup>
 
-class Slider extends Component{
-
-  render() {
-    return (
-      <FormGroup >
-        <Col componentClass={ControlLabel} xs={12}>
-          {this.props.args.Description}
-        </Col>
-        <Col xs={12}>
-          <FormControl
-            type="number"
-            min={this.props.args.Min}
-            max={this.props.args.Max}
-            value={this.props.args.Value}
-            onChange={(e) => this.props.onChange(e.target.value)}/>
-        </Col>
-        <Col xs={12}></Col>
-        <Col xs={12}>
-          <ReactBootstrapSlider
-            value={this.props.args.Value}
-            min={this.props.args.Min}
-            max={this.props.args.Max}
-            tooltip="hide"
-            change={(e) => this.props.onChange(e.target.value)}
-            />
-        </Col>
-      </FormGroup>
-    );
-  }
-}
-
-export default Slider;
+export default Slider
---
+++ liveviz/src/controls/Slider.test.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import Slider from './Slider'
+import { shallow } from 'enzyme'
+
+function setup(args) {
+  const props = {
+    onChange: jest.fn(),
+    args
+  }
+
+  const wrapper = shallow(<Slider {...props}/>)
+  return {
+    props,
+    wrapper
+  }
+}
+
+describe('Slider', () => {
+  let wrapper, props
+
+  const args = {
+    Description: "A cool param",
+    ActualName: "A cool name",
+    Value: 18,
+    Min: 10,
+    Max: 30
+  }
+  beforeEach(() => {
+    const s = setup(args)
+    wrapper = s.wrapper
+    props = s.props
+  })
+  it('should render with proper title', () => {
+    expect(wrapper.find('Col').first().children().first().text())
+    .toBe(args.Description)
+  })
+  it('should render a FormControl of type number', () => {
+    const elem = wrapper.find('FormControl').first()
+    expect(elem.props().type).toBe('number')
+    expect(elem.props().min).toBe(args.Min)
+    expect(elem.props().max).toBe(args.Max)
+    expect(elem.props().value).toBe(args.Value)
+  })
+  it('should call FormControl onChange with proper value', () => {
+    const elem = wrapper.find('FormControl')
+    elem.props().onChange({target:{value: 98}})
+    expect(props.onChange.mock.calls.length).toBe(1)
+    expect(props.onChange.mock.calls[0]).toEqual([98])
+  })
+  it('should render a ReactBootstrapSlider', () => {
+    const elem = wrapper.find('ReactBootstrapSlider')
+    expect(elem.props().min).toBe(args.Min)
+    expect(elem.props().max).toBe(args.Max)
+    expect(elem.props().value).toBe(args.Value)
+    expect(elem.props().tooltip).toBe("hide")
+  })
+  it('should call ReactBootstrapSlider onChange with proper value', () => {
+    const elem = wrapper.find('ReactBootstrapSlider')
+    elem.props().change({target:{value: 98}})
+    expect(props.onChange.mock.calls.length).toBe(1)
+    expect(props.onChange.mock.calls[0]).toEqual([98])
+  })
+})
