fe801b76535db317d34546ea3d9e33c0f545c26e
Yehonathan Sharvit <viebel@gmail.com>
2017-05-23 11:20:00 +0300

    separate Root

--- liveviz/src/index.js
+++ liveviz/src/index.js
@@ -1,34 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './logic/App'
-import { Provider } from 'react-redux'
-import { AppContainer } from 'react-hot-loader'
-import './styles/index.css'
-import { v4 } from 'node-uuid'
 import configureStore from './common/configureStore'
-
-
-const store = configureStore()
+import { nextApp, Root, App } from './ui/Root'
 
 const render = Component => {
   ReactDOM.render(
-    <AppContainer>
-      <Provider store={ store }>
-        <Component
-          key={ //TODO Yehonathan 2017, May 19: get rid of the key - without it hot reload doesn't work
-            v4()}/>
-        </Provider>
-      </AppContainer>
-      ,
-      document.getElementById('root')
-    )
-  }
+    <Root
+      Component={ Component }
+      store={  configureStore()}
+      />,
+    document.getElementById('root')
+  )
+}
 
-  render(App)
-
+const hotReload = () => {
   if (module.hot) {
     module.hot.accept('./logic/App', () => {
-      const NextApp = require('./logic/App').default
-      render(NextApp)
+      render(nextApp())
     })
   }
+}
+
+render(App)
+hotReload()
