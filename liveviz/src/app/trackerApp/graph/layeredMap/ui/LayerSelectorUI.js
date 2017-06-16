12d06e75079290a6bf0dd7fb5dd296cc03d0b270
elie buff <elieb@balink.net>
2017-06-16 15:32:20 +0300

    Working

--- liveviz/src/app/trackerApp/graph/layeredMap/ui/LayerSelectorUI.js
+++ liveviz/src/app/trackerApp/graph/layeredMap/ui/LayerSelectorUI.js
@@ -1,26 +1,37 @@
 import React from 'react'
 import {compose, reduce, filter, identity} from 'ramda'
-import CheckboxChoices from 'controls/Checkbox'
+import { Button, } from 'react-bootstrap'
 
 
 const arrToObj = arr =>
-reduce((acc,value) => {
-  acc[value] = true
-  return acc
-},
-{},
-arr)
+    reduce((acc,value) => {
+            acc[value] = true
+            return acc
+        },
+        {},
+        arr)
 
 const objToArr = compose(Object.keys, filter(identity))
 
 const LayerSelectorUI = ({layers, availableLayers, setLayers}) =>
-    <CheckboxChoices
-        onChange={compose(setLayers, arrToObj)}
-        args={{
-            Description: "",
-            Value: objToArr(layers),
-            ListValues: availableLayers
-        }}
-    />
+    <div className="layerSelector layeredMap_btnBlock">
+        {
+            availableLayers.map((item) => {
+                    var isSelectedClass = layers[item]? 'selected' : "";
+                   return (<Button key={item} className="layeredMap_layerIcons" bsStyle="primary">
+                        <div className={isSelectedClass + " layerSelector__btnImg layeredMap_projection_" + item.replace(" ", "")}/>
+                    </Button>)
+                }
+            )
+        }
+    </div>
+{/*<CheckboxChoices
+ onChange={compose(setLayers, arrToObj)}
+ args={{
+ Description: "",
+ Value: objToArr(layers),
+ ListValues: availableLayers
+ }}
+ />*/}
 
-  export default LayerSelectorUI
+export default LayerSelectorUI
