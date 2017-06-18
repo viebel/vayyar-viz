799ebecae86c47954501e89995609cfb169bbc93
Yehonathan Sharvit <viebel@gmail.com>
2017-06-18 17:20:45 +0300

    add tests

--- liveviz/src/app/trackerApp/graph/layeredMap/ui/LayerSelectorUI.js
+++ liveviz/src/app/trackerApp/graph/layeredMap/ui/LayerSelectorUI.js
@@ -4,38 +4,42 @@ import { Button, } from 'react-bootstrap'
 
 
 const arrToObj = arr =>
-    reduce((acc,value) => {
-            acc[value] = true
-            return acc
-        },
-        {},
-        arr)
+reduce((acc,value) => {
+  acc[value] = true
+  return acc
+},
+{},
+arr)
 
 const objToArr = compose(Object.keys, filter(identity))
 
 const LayerSelectorUI = ({layers, availableLayers, setLayers}) =>
-    <div className="layerSelector layeredMap_btnBlock">
-        {
-            availableLayers.map((item) => {
-                   var isSelectedClass = layers[item]? 'selected' : "";
-                   return (<Button key={item} className="layeredMap_layerIcons" bsStyle="primary"
-                                   onClick={ () => {
-                                       layers[item] = !layers[item];
-                                       return setLayers(layers);
-                                   }}>
-                        <div className={isSelectedClass + " layerSelector__btnImg layeredMap_projection_" + item.replace(" ", "")}/>
-                    </Button>)
-                }
-            )
-        }
-    </div>
+<div className="layerSelector layeredMap_btnBlock">
+  {
+    availableLayers.map((item) => {
+      var isSelectedClass = layers[item]? 'selected' : "";
+      return (
+        <Button
+          key={item}
+          className="layeredMap_layerIcons"
+          bsStyle="primary"
+          onClick={ () => {
+            layers[item] = !layers[item];
+            return setLayers(layers);
+          }}>
+          <div className={isSelectedClass + " layerSelector__btnImg layeredMap_projection_" + item.replace(" ", "")}/>
+        </Button>)
+      }
+    )
+  }
+</div>
 {/*<CheckboxChoices
- onChange={compose(setLayers, arrToObj)}
- args={{
- Description: "",
- Value: objToArr(layers),
- ListValues: availableLayers
- }}
- />*/}
+  onChange={compose(setLayers, arrToObj)}
+  args={{
+  Description: "",
+  Value: objToArr(layers),
+  ListValues: availableLayers
+  }}
+  />*/}
 
-export default LayerSelectorUI
+  export default LayerSelectorUI
