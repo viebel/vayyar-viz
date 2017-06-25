876812b94634b5ccd31d64ba755ad5c723817234
Yehonathan Sharvit <viebel@gmail.com>
2017-06-25 09:16:22 +0300

    Maps: add Ruler to the axis
    * Implement the ruler horizontal & vertical
    
    * Implement ruler
    
    * RulerUI: use functional programming code + add tests

--- liveviz/src/app/trackerApp/graph/layeredMap/ui/LayeredMapUI.js
+++ liveviz/src/app/trackerApp/graph/layeredMap/ui/LayeredMapUI.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import LayerMapToolbarUI from './LayerMapToolbarUI'
+import RulerUI from './RulerUI'
 
 
 import '../styles/LayeredMap.css';
@@ -12,20 +13,23 @@ import Tracker from 'components/trackermap/logic/Tracker';
 const LayeredMapUI = ({setLayers, setSlice, layers, slice, availableLayers}) => {
   return (
     <div className="layered-map">
-        <LayerMapToolbarUI
-            layers={layers}
-            slice={slice}
-            setLayers={setLayers}
-            setSlice={setSlice}
-            availableLayers={availableLayers}/>
+        <RulerUI maxVerticalValue={15} maxHorizontalValue={20}/>
+        <div className="layeredMap__drawingSection">
+            <LayerMapToolbarUI
+                layers={layers}
+                slice={slice}
+                setLayers={setLayers}
+                setSlice={setSlice}
+                availableLayers={availableLayers}/>
 
-        <div className="layeredMap__graph">
-            {
-                layers.heatmap ? <HeatMap/> : null
-            }
-            {
-                layers.target || layers.position ? <Tracker showPosition={layers.position}/> : null
-            }
+            <div className="layeredMap__graph">
+                {
+                    layers.heatmap ? <HeatMap/> : null
+                }
+                {
+                    layers.target || layers.position ? <Tracker showPosition={layers.position}/> : null
+                }
+            </div>
         </div>
     </div>
   )
