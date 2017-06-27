af8f54a1517af778aef4ca7c6a8c6ee96542eafe
elie buff <elie.buff@gmail.com>
2017-06-27 09:26:23 +0300

    Add connection

--- liveviz/src/app/AppUI.js
+++ liveviz/src/app/AppUI.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import TrackerApp from 'app/trackerApp/logic/TrackerApp'
+import Connection from './Connection';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap-theme.css'
 import 'styles/App.css'
@@ -9,12 +10,14 @@ import { Navbar} from 'react-bootstrap'
 const AppUI = ({url, status, graphKey, onConnect, updateStatus}) =>
 (
   <div className="fullHeight">
-    <Navbar inverse>
+    <Navbar inverse className="justify-content-between">
         <Navbar.Header>
           <Navbar.Brand>
             <a href="/">Vayyar Live Visualization</a>
           </Navbar.Brand>
+
         </Navbar.Header>
+        <Connection/>
     </Navbar>
     <TrackerApp
       key={ graphKey  /* use key in order to force re-mounting each time we re-connect */ }
---
+++ liveviz/src/app/ConnectionUI.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { Row, Col, Button, FormGroup, FormControl, Form} from 'react-bootstrap';
+import {merge} from 'ramda';
+
+
+class ConnectionUI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: props.url,
+    };
+  }
+  connect() {
+    this.props.onConnect(this.state.url);
+    return true;
+  }
+  handleChangeUrl(e) {
+    let url = e.target.value;
+    this.setState(merge(this.state, {url}));
+  }
+  render() {
+    return (
+        <div className="connection navbar-form navbar-left">
+            <Form className="form-inline">
+                <FormGroup controlId="url" className="connectionTitle">
+                    Connect to a sensor:
+                </FormGroup>
+                <FormGroup controlId="url">
+                    <FormControl
+                        type="text"
+                        ref="url"
+                        value={this.state.url}
+                        onChange={(e) => this.handleChangeUrl(e)}
+                        placeholder="Url of the sensor server">
+                    </FormControl>
+                </FormGroup>
+
+                <Button
+                    onClick={() => this.connect()}
+                    type="button"
+                    bsStyle="primary">
+                    Connect
+                </Button>
+            </Form>
+        </div>
+  );
+}
+}
+
+export default ConnectionUI;
